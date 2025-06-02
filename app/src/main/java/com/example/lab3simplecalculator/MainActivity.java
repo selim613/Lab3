2e77305b0a3f59329edc72ec56416dc088227ce9
Selim Naas <snaas027@uottawa.ca>
2025-06-02 06:02:26 -0400

    Added onClick functionality for all buttons

--- app/src/main/java/com/example/lab3simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/lab3simplecalculator/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.lab3simplecalculator;
 
 import android.os.Bundle;
-
+import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -21,4 +23,80 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void button00Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "0");
+    }
+
+    public void button01Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "1");
+    }
+
+    public void button02Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "2");
+    }
+
+    public void button03Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "3");
+    }
+
+    public void button04Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "4");
+    }
+
+    public void button05Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "5");
+    }
+
+    public void button06Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "6");
+    }
+
+    public void button07Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "7");
+    }
+
+    public void button08Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "8");
+    }
+
+    public void button09Click(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "9");
+    }
+
+    public void buttonDivClick(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "/");
+    }
+
+    public void buttonMultClick(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "*");
+    }
+
+    public void buttonSubClick(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "-");
+    }
+
+    public void buttonAddClick(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + "+");
+    }
+
+    public void buttonDotClick(View view) {
+        TextView eText = (TextView)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText() + ".");
+    }
+
 }
